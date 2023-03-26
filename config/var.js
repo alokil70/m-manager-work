3576758f514fd2bfc8952d32e4c9fcf9b940c396
Aleksandr <alokil70@gmail.com>
2023-03-26 17:48:32 +0300

    fix last error

--- config/var.js
+++ config/var.js
@@ -1,4 +1,4 @@
-export const URL = 'http://192.168.1.64:9090/api'
+export const URL = 'http://192.168.158.32:9090/api'
 // export const URL1 = 'http://91.211.248.196:9099/api'
 // export const URL = 'http://192.168.43.61:9090/api'
 // export const URL = 'http://192.168.43.167:9090/api'
